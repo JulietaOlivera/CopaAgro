bd74a6ea43d41cda00e485110180293c3d182bd4
JulietaOlivera <julietaoliveravilla@gmail.com>
2026-09-09 20:50:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from streamlit_autorefresh import st_autorefresh
 
 st.set_page_config(
     page_title="La Copa Agro",
-    page_icon= "logo.png",
+    page_icon= "logo3.png",
     layout="wide",
     initial_sidebar_state="expanded",
 )
